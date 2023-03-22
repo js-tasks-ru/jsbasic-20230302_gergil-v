c7313d04781b1d23439ae525f3d40e7cd61b8386
Vladimir <gergil.vladimir@yandex.com>
2023-03-22 18:57:04 +0500

    3-module: 4-task - Completed

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,10 @@
 function showSalary(users, age) {
-  // ваш код...
+  let str = "";
+  result = users.filter((item) => item.age <= age);
+  result.forEach((item, index) => {
+    let endStr = index !== result.length - 1 ? "\n" : "";
+    str += item.name + ", " + item.balance + endStr;
+  });
+
+  return str;
 }
