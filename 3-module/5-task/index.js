85c5dc6cce7c161b75db40a6825d6e8469ab7375
Vladimir <gergil.vladimir@yandex.com>
2023-03-22 19:13:54 +0500

    3-module: 5-task - Completed

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,11 @@
 function getMinMax(str) {
-  // ваш код...
+  result = str
+    .split(" ")
+    .map((item) => Number(item))
+    .filter((item) => Number.isFinite(item))
+    .sort((a, b) => a - b);
+  return {
+    min: result[0],
+    max: result[result.length - 1],
+  };
 }
