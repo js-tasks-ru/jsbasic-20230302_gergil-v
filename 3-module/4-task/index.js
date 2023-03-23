d3b8ba2280f9dd09692381f6aeb0f7c76e97d432
Vladimir <gergil.vladimir@yandex.com>
2023-03-23 18:47:30 +0500

    3-module: 4-task - Changed

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,10 +1,6 @@
 function showSalary(users, age) {
-  let str = "";
-  result = users.filter((item) => item.age <= age);
-  result.forEach((item, index) => {
-    let endStr = index !== result.length - 1 ? "\n" : "";
-    str += item.name + ", " + item.balance + endStr;
-  });
-
-  return str;
+  return (result = users
+    .filter((item) => item.age <= age)
+    .map((item) => `${item.name}, ${item.balance}`)
+    .join("\n"));
 }
