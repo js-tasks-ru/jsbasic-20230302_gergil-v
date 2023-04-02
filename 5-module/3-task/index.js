e5dd84764e701bfc316431dcafa8076deb74bc00
Vladimir <gergil.vladimir@yandex.com>
2023-04-02 18:23:31 +0500

    5-module: 3-task - Completed

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,33 @@
 function initCarousel() {
-  // ваш код...
+  const slider = document.querySelector(".carousel");
+  const sliderBlock = document.querySelector(".carousel__inner");
+  const sliderWidth = sliderBlock.offsetWidth;
+  let cnt = 1;
+  document.querySelector(".carousel__arrow_left").style.display = "none";
+
+  slider.addEventListener("click", (event) => {
+    if (!event.target.closest(".carousel__arrow")) return;
+
+    let arrow = event.target.closest(".carousel__arrow");
+
+    if (arrow.classList.contains("carousel__arrow_left")) {
+      sliderBlock.style.transform =
+        "translateX(-" + sliderWidth * (cnt - 2) + "px)";
+      cnt--;
+    } else {
+      sliderBlock.style.transform = "translateX(-" + sliderWidth * cnt + "px)";
+      cnt++;
+    }
+
+    if (cnt >= 4) {
+      document.querySelector(".carousel__arrow_right").style.display = "none";
+    } else {
+      document.querySelector(".carousel__arrow_right").style.display = "flex";
+    }
+    if (cnt == 1) {
+      document.querySelector(".carousel__arrow_left").style.display = "none";
+    } else {
+      document.querySelector(".carousel__arrow_left").style.display = "flex";
+    }
+  });
 }
